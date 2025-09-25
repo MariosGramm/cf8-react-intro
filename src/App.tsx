495adb9f24ed07f48e19e08e5b6cd470b77faf30
MariosGramm <mariosgramm21@gmail.com>
2025-09-25 20:51:20 +0300

    feat:added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import ViteIntro from "./components/ViteIntro/ViteIntro.tsx"
+// import ViteIntro from "./components/ViteIntro/ViteIntro.tsx"
+import ClassComponent from "./components/ClassComponent.tsx"
+import FunctionalComponent from "./components/FunctionalComponent"
+import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent"
 
 function App() {
   return (
       <>
-          <ViteIntro/>
-
+          {/*{<ViteIntro/>}*/}
+          <ClassComponent />
+          <FunctionalComponent/>
+          <ArrowFunctionalComponent/>
     </>
   )
 }
